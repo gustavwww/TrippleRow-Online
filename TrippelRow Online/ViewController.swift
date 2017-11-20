5f9b6388868caf8b9783d486f85c1d2e006e8f21
Gustav Wadström <gustavwadstrom@Gustavs-iMac.local>
2017-11-20 15:57:38 +0100

    Initial Commit

---
+++ TrippelRow Online/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrippelRow Online
+//
+//  Created by Gustav Wadström on 2017-11-20.
+//  Copyright © 2017 Gustav Wadström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
